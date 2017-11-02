b60fe2d2d0e17d73f95a0cb78c3a15679c486505
Ubuntu <team19@Team19Log.gflydvss4fkuvfosxfobwwovhc.zx.internal.cloudapp.net>
2017-11-02 15:58:29 +0000

    Script runs every second

--- gitupdatescript.rb
+++ gitupdatescript.rb
@@ -1,17 +1,20 @@
+
 #!/usr/bin/env ruby
 require 'fileutils'
 
-Dir.glob('**/.git').each do |path|
-  next if File.file?(path) # don't process submodules
-  start_time = Time.now
-  project_base = File.expand_path("#{path}/..")
-  puts "In #{project_base}"
-  FileUtils.cd(project_base) do
-    system('git stash')
-    puts "Update failed" unless system('git pull --rebase')
-    system('git stash pop')
+every 1.second do
+  Dir.glob('**/.git').each do |path|
+    next if File.file?(path) # don't process submodules
+    start_time = Time.now
+    project_base = File.expand_path("#{path}/..")
+    puts "In #{project_base}"
+    FileUtils.cd(project_base) do
+      system('git stash')
+      puts "Update failed" unless system('git pull --rebase')
+      system('git stash pop')
+    end
+    end_time = Time.now
+    puts "Done in #{end_time - start_time} sec"
+    puts
   end
-  end_time = Time.now
-  puts "Done in #{end_time - start_time} sec"
-  puts
 end
